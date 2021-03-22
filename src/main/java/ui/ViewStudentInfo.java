66589b87b060cd49540d4cc2d916d67b52eddd2a
ZyTnT <Yutong_Zhen@126.com>
2021-03-22 11:08:21 +0800

    ZYT

--- src/main/java/ui/ViewStudentInfo.java
+++ src/main/java/ui/ViewStudentInfo.java
@@ -271,8 +271,8 @@ public class ViewStudentInfo extends javax.swing.JPanel {
         DefaultTableModel model  = (DefaultTableModel)TableStudents.getModel();
         Student stu = students.get(selectedIndex);
         firstName.setText(stu.getFirstName());
-        College.setText(stu.getLastName());
-        lastName.setText(stu.getCollege());
+        College.setText(stu.getCollege());
+        lastName.setText(stu.getLastName());
         company.setText(stu.getCompany().toString());
         totalGpa.setText(String.format("%.2f",stu.getTotalGpa()));
         startSalary.setText(String.format("%.2f",stu.getStartSalary()));
